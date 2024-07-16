fbdde90496bbc42d0d02a4b369321538ab353af9
YLBohak <165651196+YLBohak@users.noreply.github.com>
2024-07-16 17:20:01 +0300

    Project 8- Incomplete
    I am still not able to read data from the arduino, either I get an empty string or something like: b"
    The threading seems to be working but need to add a mechanism to stop the printing of arduino input after first print.

--- Project8-communication-with-the-computer/README.md
+++ Project8-communication-with-the-computer/README.md
@@ -26,11 +26,11 @@
 2. The script should have a GUI (Graphical User Interface) using PySimpleGUI library. The GUI should have a response area to display messages.
 3. The script should be able to send a number over the serial port to the device. This number will be the time in seconds that the LED on the device should light up when a button is pressed.
     - For example, if the user enters '5' in the GUI, the LED on the device should light up for 5 seconds when the button is pressed.
-
 4. The device should respond with a number (0, 1, or 2) that corresponds to a specific state:
     - '0': LED off
     - '1': button and LED on
     - '2': button off
+
 5. The script should read the response from the device and update the GUI with the corresponding message.
 6. The script should continuously read from the serial port in parallel with the PySimpleGUI main loop.
 7. The script should handle any exceptions or errors that might occur during the communication with the device.
--- Project8-communication-with-the-computer/serial_led_gui.py
+++ Project8-communication-with-the-computer/serial_led_gui.py
@@ -1,47 +1,62 @@
 import PySimpleGUI as sg
 import serial
 
+#This script should be run only after burning sketch: serial_led to the arduino
+
+#Connect with arduino. change port if insert arduino into different USB port.
 arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1) 
 
-# All the stuff inside your window.
-layout = [  [sg.Text("How long would you like the led to remain on? (answer in ms)")],
+#This dictionary stores the response to print to screen based on input from the arduino
+response_ar = {'0': 'LED off','1':'button and LED on','2':'button off'}
+
+def main():
+    # All the stuff inside your window.
+    layout = [  [sg.Text("How long would you like the led to remain on? (answer in ms)")],
             [sg.InputText(size=(30, 10))],
             [sg.Button('Ok')],
             [sg.Output(size=(30, 10))]]
 
-# Create the Window
-window = sg.Window('Led timed turn-off', layout)
+    # Create the Window
+    window = sg.Window('Led timed turn-off', layout)
 
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    # Constantly read events and values given by user
-    event, values = window.read()
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        # Constantly read events and values given by user
+        event, values = window.read()
+        # if user closes window, close window
+        if event == sg.WIN_CLOSED:
+            break
 
-    # if user closes window, close window
-    if event == sg.WIN_CLOSED:
-        break
+        #If user wants us to process his/her input
+        if event == 'Ok':
+            if not values[0].isnumeric():
+                print('wrong input type, please enter number [s]')
+            else:
+                print('thank you for you input', values[0], '[ms]')
+                #Send to the arduino here
+                arduino.write(bytes(values[0], 'utf-8')) 
+            
+        window.start_thread(lambda : write_read(window),'-thread-ended-')
+       
+        if event == 'Serial':
+            print(response_ar[str(values['Serial'])])
 
-    #If user wants us to process his/her input
-    if event == 'Ok':
-        if not values[0].isnumeric():
-            print('wrong input type, please enter number [s]')
-        else:
-            print('thank you for you input', values[0], '[ms]')
-            #Send to the arduino here
-            arduino.write(bytes(values[0], 'utf-8')) 
-
-    if event == 'Serial':
-        
     
+    window.close()
 
 
-window.close()
-
-
-def write_read(seconds): 
-    #This line generates an event that can be read by window.read() in the main section of the code
-    #Serial is the event name that we gave, ar_res is a variable that stores the arduino response
-    window.write_event_value('Serial',ar_res)
+def write_read(window): 
+    while True:
+        #This line reads the input from the arduino 
+        '''The line bellow is not working yet! Cant read input from arduino'''
+        ar_res = arduino.readline().decode()#line()('utf-8').rstrip()
+        print(ar_res)
+        #This line generates an event that can be read by window.read() in the main section of the code
+        #Serial is the event name that we gave, ar_res is a variable that stores the arduino response
+        window.write_event_value('Serial',ar_res)
 	   
 
 
+
+if __name__ == "__main__":
+    main()
