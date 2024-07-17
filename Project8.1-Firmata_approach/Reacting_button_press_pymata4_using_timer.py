ddef02a63ffcab650a43012a5e87c1855ec439fe
YLBohak <165651196+YLBohak@users.noreply.github.com>
2024-07-17 14:47:32 +0300

    Project 8.1 using timer-workin but no comments and no gui
    Still need to add comments and gui

--- Project8.1-Firmata_approach/Reacting_button_press_pymata4_using_timer.py
+++ Project8.1-Firmata_approach/Reacting_button_press_pymata4_using_timer.py
@@ -2,28 +2,49 @@ import PySimpleGUI as sg
 import sys
 from pymata4 import pymata4
 import time
+import threading
 
 board = pymata4.Pymata4(com_port="COM4")
 
 LED_PIN = 4
 board.set_pin_mode_digital_output(LED_PIN)
-BUTTON_PIN = 2 #6 shorted to 2
-board.set_pin_mode_digital_input(BUTTON_PIN)
+BUTTON_PIN = 6 #
+
+
+CB_PIN_MODE = 0
+CB_PIN = 1
+CB_VALUE = 2
+CB_TIME = 3
 
 #Time for timer untill shut off
 t = 5 
 
+def led_off():
+    print('Led is off')
+    board.digital_write(LED_PIN,0)
 
 
-
-
-#arduino loop
-while True:
-    #read button and see if on
-    on_off = board.digital_read(BUTTON_PIN)
-    if on_off[0] == 1:
-        print('Button is on')
+def button_fun(data):
+    if data[CB_VALUE] == 1:
+        print('Led is on')
         #led_timer.start()
-        on_off = board.digital_write(LED_PIN,1)
-        time.sleep(t)
-        on_off = board.digital_write(LED_PIN,0)
+        board.digital_write(LED_PIN,1)
+        timer = threading.Timer(5.0, led_off)
+        # Start the timer
+        timer.start()
+
+
+
+board.set_pin_mode_digital_input(BUTTON_PIN, callback=button_fun)
+
+while True:
+    time.sleep(0.1)
+
+
+
+
+
+
+
+
+
