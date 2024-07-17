b33ee50a0410feb3ccef296916980c01f0acd843
YLBohak <165651196+YLBohak@users.noreply.github.com>
2024-07-17 15:07:54 +0300

    Project 8.1- no gui added comments
    Added clarifying comments explaining the code in the previous commit.

--- Project8.1-Firmata_approach/Reacting_button_press_pymata4_using_timer.py
+++ Project8.1-Firmata_approach/Reacting_button_press_pymata4_using_timer.py
@@ -8,9 +8,9 @@ board = pymata4.Pymata4(com_port="COM4")
 
 LED_PIN = 4
 board.set_pin_mode_digital_output(LED_PIN)
-BUTTON_PIN = 6 #
+BUTTON_PIN = 6 #We no longer need to short it as we are not using an arduino interrupt anymore
 
-
+#lables defined to make accessing the different components of the aray data (see below)
 CB_PIN_MODE = 0
 CB_PIN = 1
 CB_VALUE = 2
@@ -25,19 +25,26 @@ def led_off():
 
 
 def button_fun(data):
+    #data[2] is the value on/off
     if data[CB_VALUE] == 1:
         print('Led is on')
         #led_timer.start()
         board.digital_write(LED_PIN,1)
+        #Timer from the threading package is defined such that it can call a function directly 
+        #when the defined time elapsed
         timer = threading.Timer(5.0, led_off)
         # Start the timer
         timer.start()
 
 
-
+#Needs to be defined after the function used for the calback is defined. The callback uses a separate thread 
+#for the function that is called, so we don't need to define it ourselves. 
+#The callback is defined as part of the pymata4 package, such that the array data is returned.
+#see: https://github.com/MrYsLab/pymata4/blob/master/examples/digital_input.py, def the_callback(data):
 board.set_pin_mode_digital_input(BUTTON_PIN, callback=button_fun)
 
 while True:
+    #Add this "delay" in the loop so that while is interupted once in a while to avoid crashing.
     time.sleep(0.1)
 
 
