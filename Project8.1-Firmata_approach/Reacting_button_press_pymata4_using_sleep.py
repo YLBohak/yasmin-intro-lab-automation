9b769646bd95a3d4e77e8821e8c453748e093722
YLBohak <165651196+YLBohak@users.noreply.github.com>
2024-07-17 14:07:42 +0300

    project 8.1 using sleep

---
+++ Project8.1-Firmata_approach/Reacting_button_press_pymata4_using_sleep.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+import sys
+from pymata4 import pymata4
+import time
+
+board = pymata4.Pymata4(com_port="COM4")
+
+LED_PIN = 4
+board.set_pin_mode_digital_output(LED_PIN)
+BUTTON_PIN = 2 #6 shorted to 2
+board.set_pin_mode_digital_input(BUTTON_PIN)
+
+#Time for timer untill shut off
+t = 5 
+
+while True:
+    #read button and see if on
+    on_off = board.digital_read(BUTTON_PIN)
+    if on_off[0] == 1:
+        print('Button is on')
+        #led_timer.start()
+        on_off = board.digital_write(LED_PIN,1)
+        time.sleep(t)
+        on_off = board.digital_write(LED_PIN,0)
