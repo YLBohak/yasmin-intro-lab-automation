a4c92e4d64fe329871d79b0edf25afdcba362182
YLBohak <165651196+YLBohak@users.noreply.github.com>
2024-07-18 12:57:41 +0300

    Final project- accelerometer controlling the servo
    Added Servo

---
+++ Project11-Final_Project/Final_project_accelometer_servo/Final_project_accelometer_servo.ino
@@ -0,0 +1,50 @@
+
+//This is the package for the Accelerometer (I installed)
+#include "LIS3DHTR.h"
+//Needs this package (already installed?)
+#include <Wire.h>
+//import servo package
+#include <Servo.h>
+//Define the variable LIS as type LIS3DHTR<TwoWire>
+LIS3DHTR<TwoWire> LIS; //IIC
+//Changed name Wire to WIRE
+#define WIRE Wire
+float yAcc;
+int val;
+float valMap;
+// create Servo object to control a servo
+Servo myservo;  
+
+
+void setup() {
+  //set up connection:
+  Serial.begin(115200);
+  LIS.begin(WIRE,0x19); //IIC init
+  //I don't think that we want temperature control so I tried to shut it off
+  LIS.closeTemp();
+  //setFullScaleRange sets the full range of the accelometer --> defines how much acceleration the sensor cna measure
+  //LIS3DHTR_RANGE_2G means +-2 gravitational acceleration
+  LIS.setFullScaleRange(LIS3DHTR_RANGE_2G); 
+  //provide output measurements at this rate
+  LIS.setOutputDataRate(LIS3DHTR_DATARATE_50HZ); 
+  //attaches the servo on pin 7 to the Servo object
+  myservo.attach(7);  
+}
+
+void loop() {
+  if (!LIS)
+  {
+    Serial.println("LIS3DHTR didn't connect.");
+  }
+  //calculate the angle from the y-axis output of the accelerometer (-1 to 1)
+  //I chose to use the y-axis = board hold in front of your face and move left right (displaces the servo the least)
+  yAcc = LIS.getAccelerationY();
+  Serial.print("y:"); Serial.print(LIS.getAccelerationY()); Serial.print("  ");
+  //the y value is converted into a range between 0 and 2000, this will enable me to work with map
+  valMap = (yAcc+1)*1000;
+  //converts value from gravitational acceleration into angles between 0 and 150
+  val = map(valMap, 0, 2000, 0, 150);
+  Serial.print("angle:"); Serial.print(valMap); Serial.print(" "); Serial.println(val);
+  //write the value val (angle) to the servo
+  myservo.write(val);  
+}
