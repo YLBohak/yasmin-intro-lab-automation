bbdfcbe9e78a6da0b41ad9d33b9f50766a151823
YLBohak <165651196+YLBohak@users.noreply.github.com>
2024-07-18 13:10:48 +0300

    Final project-OLED screen
    The screen currently displays a value based on the analog value from the rotary potentiometer.

---
+++ Project11-Final_Project/Final_project-OLED_screen/Final_project-OLED_screen.ino
@@ -0,0 +1,27 @@
+//This script is based on: https://sensorkit.arduino.cc/sensorkit/module/lessons/lesson/10-the-oled-screen
+
+
+
+//Installed this package
+#include "Arduino_SensorKit.h"
+
+
+
+void setup() {
+  Oled.begin();
+  // Sets the rotation of the screen
+  Oled.setFlipMode(true); 
+
+}
+
+void loop() {
+  int random_value = analogRead(A0);
+  Oled.setFont(u8x8_font_chroma48medium8_r); 
+  Oled.setCursor(0, 33);    // Set the Coordinates 
+  Oled.print("The angle is:");   
+  Oled.print(random_value); // Print the Values  
+  Oled.refreshDisplay();    // Update the Display 
+
+  
+
+}
